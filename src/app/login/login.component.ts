90d6741771a81d86740c1943e741b2218d1b9e63
rodrigogk87 <rodrigogk87@gmail.com>
2017-11-01 01:53:58 -0300

    Validacion Angular del Login
    Se modificaron los componentes login.component.ts y login.component.html para agregar la validacion por angular, tmb se cambio el lugar donde se muestran los errores de validacion ahora se muestra arriba del input.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from "@angular/router";
+import {User} from '../models/user';
+import {UserService} from '../services/user.service';
+import { FormErrorsService } from '../services/form-errors.service';
+import { FormInterface } from '../interfaces/FormInterface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class loginComponent implements OnInit {
-
-  constructor() { }
+export class loginComponent implements OnInit,FormInterface {
+  public user: User;
+  public errors = new Map();
+  public isloading:boolean=false;
+  public getTraslatedFormName = {
+	  'name':'Nombre',
+	  'email':'Email',
+	  'password':'Password',
+  }  
+  constructor(private userService: UserService,private _router: Router, private formErrorsService: FormErrorsService) { 
+ 	this.user = new User('', '', ''); 
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(formLogInUser) {
+		this.errors= new Map();
+		
+		/* Validacion Angular */
+		Object.keys(formLogInUser.controls).forEach( (control_name) => {
+			 if(formLogInUser.controls[control_name].errors!=null){
+				Object.keys(formLogInUser.controls[control_name].errors).forEach( (control_error) => {
+					let errormessage = this.formErrorsService.errorMessages[control_error]( 
+																				this.getTraslatedFormName[control_name],
+																				formLogInUser.controls[control_name].errors
+																				);
+					if(!this.errors.has(control_name))
+					this.errors.set(control_name,errormessage);
+				});
+			 }
+		});
+		
+		/* Si formulario valida hace llamada ajax */
+		if(formLogInUser.valid){
+			console.log(this.user);
+		}
+    }
 }
